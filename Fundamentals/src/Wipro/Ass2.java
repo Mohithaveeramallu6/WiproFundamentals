3fc78889b436090685c553b051e10e19ba71daf7
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-25 12:17:50 +0530

    1st time commiting

---
+++ Fundamentals/src/Wipro/Ass2.java
@@ -0,0 +1,7 @@
+package Wipro;
+
+public class Ass2 {
+	public static void main(String args[]){
+		System.out.println("welcome john");
+		}
+}
---
+++ Fundamentals/src/Wipro/Two.java
@@ -0,0 +1,13 @@
+package Wipro;
+
+public class Two {
+	public static void main(String args[]){
+		int i=Integer.parseInt(args[0]);
+		if(i==0)
+		System.out.println("zero");
+		else if(i>0)
+		System.out.println("positive");
+		else
+		System.out.println("negative");
+		}
+		}
